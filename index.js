dafa2e0b8fff55fb029bab888acc09546d60391c
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-12-31 10:36:49 +0200

    frontend build

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('body', (req, res) => {
 app.use(bodyParser.json())
 app.use(morgan(':method :url :body :status :res[content-length] - :response-time ms'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     {
