7bb8fb8d86406f3082e78d6e5cda9aaa2931e505
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-12-31 09:15:55 +0200

    t41

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/info', (req,res) => {
+  res.send(
+    `<div><p>puhelinluettelossa ${persons.length} henkilön tiedot</p><p>${new Date()}</p></div>`
+  )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
