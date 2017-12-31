c6b82c43986f66c76003a97499fbb4951ba71723
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-12-31 09:18:56 +0200

    t42

--- index.js
+++ index.js
@@ -28,6 +28,18 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/info', (req,res) => {
   res.send(
     `<div><p>puhelinluettelossa ${persons.length} henkilön tiedot</p><p>${new Date()}</p></div>`
