d52f3d0097b6dbc86b7de712b252fa32b647ba44
techstalgist <mikko.i.ruuskanen@gmail.com>
2017-12-31 09:49:27 +0200

    t42

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
       "name": "Martti Tienari",
       "number": "040-123456",
@@ -38,6 +38,13 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 app.get('/info', (req,res) => {
